bf9a8813bfc08d2b1dfb26ceaae15c96631ace14
Themis_Rem <72138115+ThemisRem@users.noreply.github.com>
2022-12-18 13:48:02 +0200

    Add files via upload

---
+++ HappyBirthday/Old.py
@@ -0,0 +1,12 @@
+x=input("How Old Are You?\n")
+if int(x) > 12:
+ print("you are a big boy")
+else: 
+ print("you are not a big boy")
+e=input("Did Anyone Give You A Pressent?\n")
+if e == ("yes"): 
+ print ("noice")
+else: 
+ print ("that's sad")
+
+print ("Also I Almost Forgot Happy Birthday!!!")
